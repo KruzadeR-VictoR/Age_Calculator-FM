7ece704d6edff8c28df5ccb30da2c12c574e3d82
KruzadeR-VictoR <victorbiju31@gmail.com>
2023-04-12 11:19:37 +0530

    fixed accessibility issues

--- src/Page.jsx
+++ src/Page.jsx
@@ -3,51 +3,51 @@ import App from "./App";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 
 function Page() {
-  // const theme = extendTheme({
-  //   textStyles: {
-  //     label: {
-  //       letterSpacing: "3px",
-  //       color: "hsl(0, 1%, 44%)",
-  //       // fontWeight:'normal',
-  //       //   fontSize:".3rem",
-  //     },
-  //     error:{
-  //       color:'hsl(0, 100%, 67%)'
-  //     },
-  //     result: {
-  //       fontWeight: "extraBold",
-  //       fontStyle: "italic",
-  //       color: "hsl(259, 100%, 65%)",
-  //     },
-  //     res_text: {
-  //       fontWeight: "Bold",
-  //       fontStyle: "italic",
-  //     },
-  //   },
-  //   layerStyles: {
-  //     input: {
-  //       // htmlSize:'4',
-  //       width: "7rem",
-  //     },
-  //   },
-  //   fontWeights: {
-  //     normal: "400",
-  //     bold: "700",
-  //     extraBold: "800",
-  //   },
-  //   breakpoints: {
-  //     sm: "30em", // 480px
-  //     md: "48em", // 768px
-  //     lg: "62em", // 992px
-  //     xl: "80em", // 1280px
-  //     "2xl": "96em", // 1536px
-  //   },
-  // });
+  const theme = extendTheme({
+    textStyles: {
+      label: {
+        letterSpacing: "3px",
+        color: "hsl(0, 1%, 44%)",
+        // fontWeight:'normal',
+        //   fontSize:".3rem",
+      },
+      error:{
+        color:'hsl(0, 100%, 67%)'
+      },
+      result: {
+        fontWeight: "extraBold",
+        fontStyle: "italic",
+        color: "hsl(259, 100%, 65%)",
+      },
+      res_text: {
+        fontWeight: "Bold",
+        fontStyle: "italic",
+      },
+    },
+    layerStyles: {
+      input: {
+        // htmlSize:'4',
+        width: "7rem",
+      },
+    },
+    fontWeights: {
+      normal: "400",
+      bold: "700",
+      extraBold: "800",
+    },
+    breakpoints: {
+      sm: "30em", // 480px
+      md: "48em", // 768px
+      lg: "62em", // 992px
+      xl: "80em", // 1280px
+      "2xl": "96em", // 1536px
+    },
+  });
 
   return (
-    // <ChakraProvider theme={theme}>
+    <ChakraProvider theme={theme}>
       <App />
-    // </ChakraProvider>
+    </ChakraProvider>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,53 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import Page from "./Page";
-import { ChakraProvider, extendTheme } from "@chakra-ui/react";
-
-const theme = extendTheme({
-  textStyles: {
-    label: {
-      letterSpacing: "3px",
-      color: "hsl(0, 1%, 44%)",
-      // fontWeight:'normal',
-      //   fontSize:".3rem",
-    },
-    error:{
-      color:'hsl(0, 100%, 67%)'
-    },
-    result: {
-      fontWeight: "extraBold",
-      fontStyle: "italic",
-      color: "hsl(259, 100%, 65%)",
-    },
-    res_text: {
-      fontWeight: "Bold",
-      fontStyle: "italic",
-    },
-  },
-  layerStyles: {
-    input: {
-      // htmlSize:'4',
-      width: "7rem",
-    },
-  },
-  fontWeights: {
-    normal: "400",
-    bold: "700",
-    extraBold: "800",
-  },
-  breakpoints: {
-    sm: "30em", // 480px
-    md: "48em", // 768px
-    lg: "62em", // 992px
-    xl: "80em", // 1280px
-    "2xl": "96em", // 1536px
-  },
-});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <Page />
-    </ChakraProvider>
+    <Page />
   </React.StrictMode>
 );
