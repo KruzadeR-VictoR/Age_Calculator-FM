244b4aabf988a140ca3e2afecb26970529707887
KruzadeR-VictoR <victorbiju31@gmail.com>
2023-04-12 11:24:44 +0530

    fixed accessibility issues

--- src/Validations/Validation.js
+++ src/Validations/Validation.js
@@ -1,16 +1,19 @@
 import * as Yup from "yup";
 
+const year = new Date().getFullYear();
+// console.log(year);
+
 export const ageSchema = Yup.object({
   day: Yup.number()
-    .min(1)
+    .min(1, "must be a valid month")
     .max(31, "must be a valid day")
     .required("This field is required"),
   month: Yup.number()
-    .min(1)
+    .min(1, "must be a valid month")
     .max(12, "must be a valid month")
     .required("This field is required"),
   year: Yup.number()
-    .min(4)
-    .max(2023, "must be a valid year")
+    .min(1000, "must be a valid month")
+    .max(year, "must be a valid year")
     .required("This field is required"),
 });
